d0bf8d44c3fa1961480d1f90724e812c4767b458
djmkj <3213330549@qq.com>
2021-07-30 00:07:22 +0800

    商品模型

---
+++ src/main/java/com/example/demo/service/impl/ItemServiceImpl.java
@@ -0,0 +1,84 @@
+package com.example.demo.service.impl;
+
+import com.example.demo.DataObject.ItemDo;
+import com.example.demo.DataObject.ItemStockDo;
+import com.example.demo.dao.ItemDoMapper;
+import com.example.demo.dao.ItemStockDoMapper;
+import com.example.demo.error.BusinessException;
+import com.example.demo.error.EmBusinessError;
+import com.example.demo.service.ItemService;
+import com.example.demo.service.model.ItemModel;
+import com.example.demo.validator.ValidationResult;
+import com.example.demo.validator.ValidatorImpl;
+import org.springframework.beans.BeanUtils;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
+import org.springframework.transaction.annotation.Transactional;
+
+import java.util.List;
+
+@Service
+public class ItemServiceImpl implements ItemService {
+
+    @Autowired
+    private ValidatorImpl validator;
+
+    @Autowired
+    private ItemDoMapper itemDoMapper;
+
+    @Autowired
+    private ItemStockDoMapper itemStockDoMapper;
+
+    private ItemDo convertItemDoFromItemModel(ItemModel itemModel){
+        if (itemModel == null){
+            return null;
+        }
+        ItemDo itemDo = new ItemDo();
+        BeanUtils.copyProperties(itemModel,itemDo);
+        itemDo.setPrice(itemModel.getPrice().doubleValue());
+        return itemDo;
+    }
+
+    private ItemStockDo convertItemStockDoFromItemModel(ItemModel itemModel){
+        if (itemModel == null){
+            return null;
+        }
+        ItemStockDo itemStockDo = new ItemStockDo();
+        itemStockDo.setItemId(itemModel.getId());
+        itemStockDo.setStock(itemModel.getStock());
+        return itemStockDo;
+    }
+
+    @Override
+    @Transactional
+    public ItemModel createItem(ItemModel itemModel) throws BusinessException {
+        //校验入参
+        ValidationResult result = validator.validate(itemModel);
+        if (result.isHasErrors()){
+            throw new BusinessException(EmBusinessError.PARAMETER_VALIDATION_ERROR,result.getErrMsg());
+        }
+
+        //转化itemmodel -> dataobject
+        ItemDo itemDo = this.convertItemDoFromItemModel(itemModel);
+
+        //写入数据库
+        itemDoMapper.insertSelective(itemDo);
+        itemModel.setId(itemDo.getId());
+        ItemStockDo itemStockDo = this.convertItemStockDoFromItemModel(itemModel);
+        itemStockDoMapper.insertSelective(itemStockDo);
+
+        //返回创建完成的对象
+        return this.getItemById(itemModel.getId());
+    }
+
+    @Override
+    public List<ItemModel> listItem() {
+        return null;
+    }
+
+    @Override
+    public ItemModel getItemById(Integer id) {
+
+        return null;
+    }
+}
